0111c195e3fb5ec9ff6ed2ed1444af31031658b3
Solarint <fear231@gmail.com>
2023-05-03 14:16:43 -0500

    Refactored dynamic lean

--- SAIN Movement/Components/LeanComponent.cs
+++ SAIN Movement/Components/LeanComponent.cs
@@ -3,137 +3,130 @@ using EFT;
 using Movement.Helpers;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static Movement.UserSettings.DogFight;
 
 namespace Movement.Components
 {
     public class DynamicLean : MonoBehaviour
     {
-        private bool ShouldILean
-        {
-            get
-            {
-                if (bot?.Memory?.GoalEnemy == null)
-                    return false;
-
-                if (bot?.BotState != EBotState.Active) 
-                    return false;
-
-
-                if (!LeanToggle.Value) 
-                    return false;
-
-                if (!ScavLeanToggle.Value && bot.IsRole(WildSpawnType.assault))
-                    return false;
-
-
-                if (bot.Memory.GoalEnemy.CanShoot == true || bot.Memory.IsInCover == true) 
-                    return false;
-
-                if (bot.Memory.GoalEnemy.Distance > 30f) 
-                    return false;
-
-                return true;
-            }
-        }
-        private bool ShouldIReset
-        {
-            get
-            {
-                if (Lean.Leaning)
-                {
-                    if (bot?.BotState != EBotState.Active)
-                        return true;
-
-                    if (!ScavLeanToggle.Value && bot.IsRole(WildSpawnType.assault))
-                        return true;
-
-                    if (!LeanToggle.Value)
-                        return true;
-
-                    if (bot?.Memory?.GoalEnemy == null)
-                        return true;
-
-                    if (bot.Memory.GoalEnemy.IsVisible == true || bot.Memory.GoalEnemy.CanShoot)
-                        return true;
-                }
-                return false;
-            }
-        }
-        public bool StartingLean { get; private set; }
-        public bool Resetting { get; private set; }
-
         private void Awake()
         {
             bot = GetComponent<BotOwner>();
 
             Logger = BepInEx.Logging.Logger.CreateLogSource(nameof(DynamicLean) + $": {bot.name}: ");
 
-            Resetting = false;
-            StartingLean = false;
             Lean = new Corners.FindDirectionToLean(bot);
 
-            StartCoroutine(ContinuousLean());
+            StartCoroutine(FindLeanAngleLoop());
+
+            StartCoroutine(LeanConditionCheckLoop());
         }
 
-        public IEnumerator Dispose()
+        public void Dispose()
         {
             StopAllCoroutines();
-            Lean = null;
-            yield break;
+            Destroy(this);
         }
 
-        private IEnumerator ContinuousLean()
+        private IEnumerator LeanConditionCheckLoop()
         {
             while (true)
             {
-                if (ShouldIReset && !Resetting)
+                if (ShouldBotLean)
                 {
-                    Resetting = true;
+                    StartCoroutine(ExecuteLean(0.25f));
+                }
+                else if (!ShouldBotLean)
+                {
                     StartCoroutine(ResetLeanAfterDuration(Random.Range(1f, 2f)));
                 }
-                else if (ShouldILean && !StartingLean)
-                {
-                    StartingLean = true;
-                    StartCoroutine(ExecuteLean(0.5f));
-                }
 
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForEndOfFrame();
             }
         }
 
         public IEnumerator ExecuteLean(float delay)
         {
-            float leanAngle = Lean.FindLeanAngle(10f);
-
-            yield return new WaitForSeconds(delay);
-
-            StartingLean = false;
-
-            if (ShouldILean && !ShouldIReset)
+            if (!StartingLean)
             {
-                bot.GetPlayer.MovementContext.SetTilt((leanAngle > 0f) ? 5 : -5, false);
+                StartingLean = true;
+
+                yield return new WaitForSeconds(delay);
+
+                StartingLean = false;
+
+                if (ShouldBotLean)
+                {
+                    SetLean((LeaningRight ? 5 : -5), true);
+                }
+            }
+        }
+
+        private IEnumerator FindLeanAngleLoop()
+        {
+            while (true)
+            {
+                if (!EnemyVisibleOrShootable)
+                {
+                    LeanAngle = Lean.FindLeanAngle(20f);
+                    yield return new WaitForSeconds(0.2f);
+                }
+
+                yield return new WaitForEndOfFrame();
             }
         }
 
         private IEnumerator ResetLeanAfterDuration(float duration)
         {
-            yield return new WaitForSeconds(duration);
-            Resetting = false;
-
-            if (!ShouldILean && ShouldIReset)
+            if (!Resetting)
             {
-                bot.GetPlayer.MovementContext.SetTilt(0f, false);
+                Resetting = true;
 
-                Lean.Leaning = false;
+                yield return new WaitForSeconds(duration);
+
+                Resetting = false;
+
+                if (!ShouldBotLean)
+                {
+                    SetLean(0f, false);
+                }
             }
         }
 
+        private void SetLean(float num, bool value)
+        {
+            if (LastLeanNum != num)
+            {
+                LastLeanNum = num;
+
+                bot.GetPlayer.MovementContext.SetTilt(num, false);
+
+                Logger.LogDebug($"Leaning Right: [{LeaningRight}] so Lean Input is {LastLeanNum} because [{LeanAngle}]");
+            }
+            Lean.Leaning = value;
+        }
+
+        private bool ShouldBotLean => (BotActive || !GoalEnemyNull || !EnemyVisibleOrShootable || AllowLean || AllowScavLean) && !BotInCover;
+        private bool EnemyFar => !GoalEnemyNull && bot.Memory.GoalEnemy.Distance > 50f;
+        private bool BotActive => bot?.BotState == EBotState.Active;
+        private bool AllowScavLean => ScavLeanToggle.Value && Scav;
+        private bool AllowLean => LeanToggle.Value && !Scav;
+        private bool Scav => bot.IsRole(WildSpawnType.assault);
+        private bool EnemyVisibleOrShootable => EnemyVisible || EnemyCanShoot;
+        private bool EnemyVisible => !GoalEnemyNull && bot.Memory.GoalEnemy.IsVisible;
+        private bool EnemyCanShoot => !GoalEnemyNull && bot.Memory.GoalEnemy.CanShoot;
+        private bool GoalEnemyNull => bot?.Memory?.GoalEnemy == null;
+        private bool BotInCover => bot.Memory.IsInCover;
+        public bool LeaningRight => LeanAngle > 0f;
+        public float LeanAngle { get; private set; }
+
         private Corners.FindDirectionToLean Lean;
-
         private BotOwner bot;
-
         protected ManualLogSource Logger;
+        private bool StartingLean;
+        private bool Resetting;
+        private float LastLeanNum = 0f;
 
     }
 }
