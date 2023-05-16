47b8024bdf264e79204267d32872c3d6b1d2852a
Solarint <fear231@gmail.com>
2023-05-15 20:37:48 -0500

    broke up Hide Coroutine into helpers

--- SAIN Movement/Components/HideComponent.cs
+++ SAIN Movement/Components/HideComponent.cs
@@ -55,12 +55,12 @@ namespace SAIN.Components
 
         public float HideSensitivity = 0;
         public float MinPlayerDistance = 5f;
-        public float MinObstacleHeight = 1.55f;
+        public float MinObstacleHeight = 0.5f;
         public float UpdateFrequency = 0.25f;
 
         private Coroutine TakeCoverCoroutine;
 
-        private Collider[] Colliders = new Collider[50]; // more is less performant, but more options
+        private readonly Collider[] Colliders = new Collider[30]; // more is less performant, but more options
 
         private IEnumerator Hide(Player enemy)
         {
@@ -68,74 +68,100 @@ namespace SAIN.Components
 
             while (true)
             {
-                for (int i = 0; i < Colliders.Length; i++)
-                {
-                    Colliders[i] = null;
-                }
-
-                int hits = Physics.OverlapSphereNonAlloc(SAIN.BotOwner.Transform.position, 30f, Colliders, HidableLayers);
-
-                int hitReduction = 0;
-                for (int i = 0; i < hits; i++)
-                {
-                    if (Vector3.Distance(Colliders[i].transform.position, enemy.Transform.position) < MinPlayerDistance || Colliders[i].bounds.size.y < MinObstacleHeight)
-                    {
-                        Colliders[i] = null;
-                        hitReduction++;
-                    }
-                }
-
-                hits -= hitReduction;
+                GetColliders(enemy.Transform.position, out int hits);
 
                 System.Array.Sort(Colliders, ColliderArraySortComparer);
 
                 for (int i = 0; i < hits; i++)
                 {
-                    if (NavMesh.SamplePosition(Colliders[i].transform.position, out NavMeshHit hit, 3f, NavMesh.AllAreas))
+                    if (CheckSides(Colliders[i], enemy.Transform.position, out NavMeshHit PlaceToGo, 3f))
                     {
-                        if (!NavMesh.FindClosestEdge(hit.position, out hit, NavMesh.AllAreas))
+                        NavMeshPath Path = new NavMeshPath();
+                        if (NavMesh.CalculatePath(SAIN.BotOwner.Transform.position, PlaceToGo.position, NavMesh.AllAreas, Path))
                         {
-                            Logger.LogError($"Unable to find edge close to {hit.position}");
-                        }
-
-                        if (Vector3.Dot(hit.normal, (enemy.Transform.position - hit.position).normalized) < HideSensitivity)
-                        {
-                            NavMeshPath Path = new NavMeshPath();
-                            if (NavMesh.CalculatePath(SAIN.BotOwner.Transform.position, hit.position, NavMesh.AllAreas, Path) && Path.status == NavMeshPathStatus.PathComplete)
+                            if (Path.status == NavMeshPathStatus.PathComplete)
                             {
-                                SAIN.BotOwner.GoToPoint(hit.position, false);
+                                SAIN.BotOwner.GoToPoint(PlaceToGo.position, false);
                                 break;
                             }
                         }
-                        else
-                        {
-                            // Since the previous spot wasn't facing "away" enough from teh target, we'll try on the other side of the object
-                            if (NavMesh.SamplePosition(Colliders[i].transform.position - (enemy.Transform.position - hit.position).normalized * 2, out NavMeshHit hit2, 2f, NavMesh.AllAreas))
-                            {
-                                if (!NavMesh.FindClosestEdge(hit2.position, out hit2, NavMesh.AllAreas))
-                                {
-                                    Logger.LogError($"Unable to find edge close to {hit2.position} (second attempt)");
-                                }
-
-                                if (Vector3.Dot(hit2.normal, (enemy.Transform.position - hit2.position).normalized) < HideSensitivity)
-                                {
-                                    NavMeshPath Path = new NavMeshPath();
-                                    if (NavMesh.CalculatePath(SAIN.BotOwner.Transform.position, hit2.position, NavMesh.AllAreas, Path) && Path.status == NavMeshPathStatus.PathComplete)
-                                    {
-                                        SAIN.BotOwner.GoToPoint(hit2.position, false);
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Logger.LogError($"Unable to find NavMesh near object {Colliders[i].name} at {Colliders[i].transform.position}");
                     }
                 }
                 yield return Wait;
             }
+        }
+
+        private void GetColliders(Vector3 enemyPosition, out int hits)
+        {
+            for (int i = 0; i < Colliders.Length; i++)
+            {
+                Colliders[i] = null;
+            }
+
+            hits = Physics.OverlapSphereNonAlloc(SAIN.BotOwner.Transform.position, 30f, Colliders, HidableLayers);
+
+            int hitReduction = 0;
+            for (int i = 0; i < hits; i++)
+            {
+                if (Vector3.Distance(Colliders[i].transform.position, enemyPosition) < MinPlayerDistance || Colliders[i].bounds.size.y < MinObstacleHeight)
+                {
+                    Colliders[i] = null;
+                    hitReduction++;
+                }
+            }
+
+            hits -= hitReduction;
+        }
+
+        private bool CheckSides(Collider collider, Vector3 enemyPosition, out NavMeshHit placeToGo, float navSampleSize = 2f)
+        {
+            placeToGo = new NavMeshHit();
+            if (NavMesh.SamplePosition(collider.transform.position, out NavMeshHit hit, navSampleSize, NavMesh.AllAreas))
+            {
+                if (!NavMesh.FindClosestEdge(hit.position, out hit, NavMesh.AllAreas))
+                {
+                    Logger.LogError($"Unable to find edge close to {hit.position}");
+                }
+
+                if (Vector3.Dot(hit.normal, (enemyPosition - hit.position).normalized) < HideSensitivity)
+                {
+                    placeToGo = hit;
+                    return true;
+                }
+                else
+                {
+                    if (CheckOppositeSide(hit, collider, enemyPosition, out NavMeshHit hit2))
+                    {
+                        placeToGo = hit2;
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                Logger.LogError($"Unable to find NavMesh near object {collider.name} at {collider.transform.position}");
+            }
+            return false;
+        }
+
+        private bool CheckOppositeSide(NavMeshHit hit, Collider collider, Vector3 enemyPosition, out NavMeshHit placeToGo, float navSampleSize = 2f)
+        {
+            if (NavMesh.SamplePosition(collider.transform.position - (enemyPosition - hit.position).normalized * 2, out NavMeshHit hit2, navSampleSize, NavMesh.AllAreas))
+            {
+                if (!NavMesh.FindClosestEdge(hit2.position, out hit2, NavMesh.AllAreas))
+                {
+                    Logger.LogError($"Unable to find edge close to {hit2.position} (second attempt)");
+                }
+
+                if (Vector3.Dot(hit2.normal, (enemyPosition - hit2.position).normalized) < HideSensitivity)
+                {
+                    placeToGo = hit2;
+                    return true;
+                }
+            }
+
+            placeToGo = hit;
+            return false;
         }
 
         public int ColliderArraySortComparer(Collider A, Collider B)
