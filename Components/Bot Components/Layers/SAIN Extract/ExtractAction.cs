dc8a765adbead2de71c7036b71c07ecb45019946
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2023-07-14 18:37:01 -0700

    Fix extraction exceptions
    - Utilize the same method for despawning bots as Donuts uses, fixes exceptions
    - Add a try/catch around Memory.Spotted, as the BSG code throws an exception sometimes

--- Components/Bot Components/Layers/SAIN Extract/ExtractAction.cs
+++ Components/Bot Components/Layers/SAIN Extract/ExtractAction.cs
@@ -13,10 +13,8 @@ namespace SAIN.Layers
 {
     internal class ExtractAction : CustomLogic
     {
-        private MethodInfo _BotUnspawn;
         public ExtractAction(BotOwner bot) : base(bot)
         {
-            _BotUnspawn = AccessTools.Method(bot.LeaveData.GetType(), "method_1");
             Logger = BepInEx.Logging.Logger.CreateLogSource(this.GetType().Name);
             SAIN = bot.GetComponent<SAINComponent>();
             Shoot = new ShootClass(bot, SAIN);
@@ -129,7 +127,12 @@ namespace SAIN.Layers
             if (ExtractTimer < Time.time)
             {
                 Logger.LogInfo($"{BotOwner.name} Extracted at {point} at {System.DateTime.UtcNow}");
-                _BotUnspawn.Invoke(BotOwner.LeaveData, null);
+
+                var botgame = Singleton<IBotGame>.Instance;
+                BotOwner.Deactivate();
+                BotOwner.Dispose();
+                botgame.BotUnspawn(BotOwner);
+                Object.Destroy(BotOwner);
             }
         }
 
