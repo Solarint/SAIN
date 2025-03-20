6e572de17aa9ea9ec335cc4e7c2103ed1455a182
jonbons <me@mattgraham.me>
2025-03-19 22:43:23 -0500

    BotsController.method_4 is now method_5

--- Patches/GenericPatches.cs
+++ Patches/GenericPatches.cs
@@ -189,7 +189,7 @@ namespace SAIN.Patches.Generic
     {
         protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.Method(typeof(BotsController), "method_4");
+            return AccessTools.Method(typeof(BotsController), "method_5");
         }
 
         [PatchPrefix]
